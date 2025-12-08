1b1129a4e3abea718d20af77d100a8d2918a8c0b
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2025-12-08 02:25:07 +0100

    fix: meta tags update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,12 +7,18 @@ import Header from "@/components/layouts/header/header";
 import Footer from "@/components/layouts/footer/footer";
 import Foot from "@/components/layouts/footer/foot";
 import QueryProvider from "./providers/QueryProvider";
+// import { Metadata } from "next";
 
 const lato = Lato({
   subsets: ["latin"],
   weight: ["100", "300", "400", "700", "900"],
   style: ["normal", "italic"],
 });
+
+// export const metadata: Metadata = {
+//   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'https://blinkers-web-app-vercel-v2-kxiu.vercel.app/'),
+//   // Your other static metadata...
+// };
 
 export default function RootLayout({
   children,
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  htmlLimitedBots:
+    /facebookexternalhit|whatsapp|twitterbot|linkedinbot|slackbot/i,
 };
 
 export default nextConfig;
